9a5639ff7d1f03b91eb1901ce34edfb8f18275fd
antonk <kapliy@gmail.com>
2010-06-25 19:47:10 +0000

    Update to ICHEP on June 25. Cutflow almost identical to Harvard folks. Still need to do: * move to extrapolated MS->ID pt * verify jet cleaning cuts

--- wmunu/trunk/tier3_submit.sh
+++ wmunu/trunk/tier3_submit.sh
@@ -2,6 +2,7 @@ anadir=/home/antonk/analysis
 outdir=/home/antonk/logs
 runs="106044 105861 106022 106047 106052  109276 109277 109278 109279 109280 109281"
 info="Wmunu  ttbar  Wtaunu Zmumu  Ztautau J0     J1     J2     J3     J4     J5"
+#runs="152844  152845  152878  152933  152994  153030  153134  153136  153159  153200  153565  153599"
 echo $info
 
 for rnum in `echo $runs`; do
