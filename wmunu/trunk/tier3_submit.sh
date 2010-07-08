e7d6408cecb122d3273c9faa07ea3a4766cde4fb
antonk <kapliy@gmail.com>
2010-07-08 19:20:35 +0000

    Minor updates to tier3 submit script to work better with expansions. Updates to remove useless verbose printouts by root. Changed root signaling for a speedup (hopefully)

--- wmunu/trunk/tier3_submit.sh
+++ wmunu/trunk/tier3_submit.sh
@@ -1,21 +1,30 @@
 anadir=/home/antonk/analysis
 outdir=/home/antonk/output
-DATADIR=/pnfs/uct3/data/users/antonk/ANALYSIS/ICHEP_DATA
-MCDIR=/pnfs/uct3/data/users/antonk/ANALYSIS/ICHEP_MC
+DATADIR="/pnfs/uct3/data/users/antonk/ANALYSIS/ICHEP_DATA/*period*/*root*"
+DATADIRABC="/pnfs/uct3/data/users/antonk/ANALYSIS/ICHEP_DATA/*period[ABC]*/*root*"
+MCDIR="/pnfs/uct3/data/users/antonk/ANALYSIS/ICHEP_MC"
 
 id=0
-for dsampledir in $DATADIR; do
-    dsample=periodABC
-    echo "Submitting job $id: $dsample"
-    mkdir -p $outdir/$dsample
-    qsub -v anadir=${anadir},inpdir="${dsampledir}/*",outdir=${outdir}/$dsample,extras="--data" -N looper${id} -o $outdir/$dsample/log.stdout -e $outdir/$dsample/log.stderr pbs.sh
-    ((id++))
-done
+dsampledir="$DATADIRABC"
+dsample=periodABC
+echo "Submitting job $id: $dsample"
+mkdir -p $outdir/$dsample
+qsub -v anadir=${anadir},inpdir="${dsampledir}",outdir=${outdir}/$dsample,extras="--data" -N looper${id} -o $outdir/$dsample/log.stdout -e $outdir/$dsample/log.stderr pbs.sh
+((id++))
+exit -1
+
+dsampledir=$DATADIR
+dsample=periodABCD1
+echo "Submitting job $id: $dsample"
+mkdir -p $outdir/$dsample
+#qsub -v anadir=${anadir},inpdir="${dsampledir}/*",outdir=${outdir}/$dsample,extras="--data" -N looper${id} -o $outdir/$dsample/log.stdout -e $outdir/$dsample/log.stderr pbs.sh
+((id++))
+#exit -1
 
 for dsampledir in `find $MCDIR -maxdepth 1 -mindepth 1 -type d | sort`; do
     dsample=`basename $dsampledir`
     echo "Submitting job $id: $dsample"
     mkdir -p $outdir/$dsample
-    qsub -v anadir=${anadir},inpdir=${dsampledir},outdir=${outdir}/$dsample,extras="" -N looper${id} -o $outdir/$dsample/log.stdout -e $outdir/$dsample/log.stderr pbs.sh
+    #qsub -v anadir=${anadir},inpdir="${dsampledir}/*root*",outdir=${outdir}/$dsample,extras="" -N looper${id} -o $outdir/$dsample/log.stdout -e $outdir/$dsample/log.stderr pbs.sh
     ((id++))
 done
