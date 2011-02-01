b32745b836c6fe678345ac21a068e9509272d29e
antonk <antonk@639f0546-53f7-3a65-3376-bd91f4a7b4f4>
2011-02-01 19:39:37 +0000

    Working version of keysfit that supports both scale and shift fits

--- roofit/branches/tutorial/FEB01.sh
+++ roofit/branches/tutorial/FEB01.sh
@@ -1,34 +1,50 @@
+#!/bin/bash
 # RooKeysPdf studies:
 data=data_data
 mc=mc_zmumu_mc_zmumu
-glb=scale
+
+for glb in scale shift; do
+xtra=""
+if [ "$glb" == "shift" ]; then xtra=1; fi
 
 tg=$mc
 reg=BB
 tag=${glb}_truth_$reg
-python keysfit.py root_all.root $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tag
+python keysfit.py root_all.root $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tag $xtra
 
 tg=$mc
 reg=CC
 tag=${glb}_truth_$reg
-python keysfit.py root_all.root $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tag
+python keysfit.py root_all.root $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tag $xtra
 
 tg=$mc
 reg=AA
 tag=${glb}_truth_$reg
-python keysfit.py root_all.root $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tag
+python keysfit.py root_all.root $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tag $xtra
 
 tg=$data
 reg=BB
 tag=${glb}_data_$reg
-python keysfit.py root_all.root $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tag
+python keysfit.py root_all.root $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tag $xtra
+
+tg=$data
+reg=Baa
+tag=${glb}_data_$reg
+python keysfit.py root_all.root $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tag $xtra
+
+tg=$data
+reg=Bcc
+tag=${glb}_data_$reg
+python keysfit.py root_all.root $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tag $xtra
 
 tg=$data
 reg=CC
 tag=${glb}_data_$reg
-python keysfit.py root_all.root $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tag
+python keysfit.py root_all.root $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tag $xtra
 
 tg=$data
 reg=AA
 tag=${glb}_data_$reg
-python keysfit.py root_all.root $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tag
+python keysfit.py root_all.root $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tg.root/dg/dg/st_z_final/$reg/graph_lpt_P_N $tag $xtra
+
+done
