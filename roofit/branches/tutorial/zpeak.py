0592172fdf4903e2f8660d56d0666c2238c04fb6
antonk <antonk@639f0546-53f7-3a65-3376-bd91f4a7b4f4>
2011-02-15 15:06:13 +0000

    Complete rewrite of zpeak fit - use command line options, multiple shapes, support unbinned TGraph input

--- roofit/branches/tutorial/zpeak.py
+++ roofit/branches/tutorial/zpeak.py
@@ -1,177 +1,275 @@
 #!/usr/bin/env python
 # fitz a single z peak
 
-import sys
-_fin = 'root_all.root'
-_hist = 'mc_zmumu_mc_zmumu.root/dg/dg/st_z_final/BB/z_m_fine'
-if len(sys.argv)>=2:
-    _fin = sys.argv[1]
-if len(sys.argv)>=3:
-    _hist = sys.argv[2]
+"""
+mc_zmumu_mc_zmumu.root/dg/dg/st_z_final/BB/graph_Z_m_eta
+data_data.root/dg/dg/st_z_final/BB/z_m_fine
+mc_zmumu_mc_zmumu.root/dg/dg/st_z_final/BB/z_m_fine
+data_data.root/dg/dg/st_z_final/BB/graph_Z_m_eta
+"""
 
-fmZ = 91.1876
+import sys,re,array
+import SimpleProgressBar
+
+def func_SCALE(xx,par):
+    return par*xx
+
+from optparse import OptionParser
+parser = OptionParser()
+# data sources
+parser.add_option("--root",dest="root",
+                  type="string", default='root_all.root',
+                  help="Input ROOT file with all histograms")
+parser.add_option("--data",dest="data",
+                  type="string", default='mc_zmumu_mc_zmumu.root/dg/dg/st_z_final/BB/graph_Z_m_eta',
+                  help="TGraph or TH1F containing reconstructed z mass")
+parser.add_option("--ndata",dest="ndata",
+                  type="int", default=1000000,
+                  help="Number of unbinned data points to load")
+parser.add_option("--nskip",dest="nskip",
+                  type="int", default=0,
+                  help="Number of data points to skip load")
+# parameters
+parser.add_option("--min",dest="min",
+                  type="float", default=70.0,
+                  help="Fit minimum")
+parser.add_option("--max",dest="max",
+                  type="float", default=110.0,
+                  help="Fit maximum")
+parser.add_option("--scale",dest="scale",
+                  type="float", default=1.0,
+                  help="Force a particular scale factor on positive muons")
+parser.add_option("--fixw", default=False,
+                  action="store_true",dest="fixw",
+                  help="Fix width of W")
+parser.add_option("--rookeys",dest="rookeys",
+                  type="string", default=None,
+                  help="ROOT file from which we load a cached RooKeysPdf")
+# choose what to run
+parser.add_option('-t',"--tag",dest="tag",
+                  type="string", default='plot',
+                  help="A tag to append to all output plots")
+parser.add_option("--batch", default=False,
+                  action="store_true",dest="batch",
+                  help="Enable batch mode (all output directly to filesystem)")
+parser.add_option("--gaus", default=False,
+                  action="store_true",dest="gaus",
+                  help="Fit simple gaussian")
+parser.add_option("--voigbg", default=False,
+                  action="store_true",dest="voigbg",
+                  help="Fit voigtian with exponential BG")
+parser.add_option("--bw", default=False,
+                  action="store_true",dest="bw",
+                  help="Fit relativistic breit-wigner convolved with gaus")
+parser.add_option("--bwfull", default=False,
+                  action="store_true",dest="bwfull",
+                  help="Fit relativistic breit-wigner (+photon exchange & interference) convolved with gaus")
+parser.add_option("--double", default=False,
+                  action="store_true",dest="double",
+                  help="Use double gaussian resolution model for convolution?")
+
+(opts, args) = parser.parse_args()
+
+mZ = 91.1876
 
 import ROOT
-from ROOT import RooWorkspace,RooArgSet,RooArgList,RooDataHist,RooAbsData,RooFormulaVar,RooRealSumPdf
-from ROOT import RooGaussModel,RooAddModel,RooRealVar
+ROOT.gROOT.SetBatch(opts.batch)
+from ROOT import RooWorkspace,RooArgSet,RooArgList,RooDataHist,RooAbsData,RooDataSet,RooFormulaVar
+from ROOT import RooGaussModel,RooAddModel,RooRealVar,RooRealSumPdf
 from ROOT import kTRUE,kFALSE,kDashed,gStyle,gPad
 from ROOT import TFile,TH1,TH1F,TH1D
 from ROOT import RooFit as RF
 w = RooWorkspace('w',kTRUE)
 
-# Z mass cannot float if we fit for scale
-mZ = '91.1876'
-# formula for Z mass:
-#formula = 'sqrt((E1+E2)^2-(pz1+pz2)^2-(pt1*cos(phi1)+pt2*cos(phi2))^2-(pt1*sin(phi1)+pt2*sin(phi2))^2)'
-
 # Voigtian with exponential
-if False:
-    minZ = '70.0'
-    maxZ = '104.0'
-    voig = "RooVoigtian::voig(x[%s,%s],mean[%s],width[0,0,5.0],sigma[1,0,5])"  #minZ,maxZ,mZ(or range)
-    exp  = "RooExponential::exp(x,expar[-0.1,-1,0])"
-    # since nfractions = npdfs, this is automatically an extended likelihood fit
-    cmd = "SUM::sum(nsig[1,0,1000000]*%s,nbg[0,0,1000000]*%s)"%(voig%(minZ,maxZ,'%s,%s,%s'%(mZ,minZ,maxZ)),exp)
-    print cmd
-    w.factory(cmd)
-    #w.var('width').setConstant(kTRUE) if w.var('width') else None
-    isExt = kTRUE
+def make_voigbg(minZ,maxZ,mZ=mZ,fixw=False):
+    """ Returns PDF and YES/NO whether it is an extended likelihood fit"""
+    cmds = []
+    cmds.append('x[%s,%s]'%(minZ,maxZ))
+    cmds.append('mz[%s]'%mZ)
+    cmds.append('width[0,0,5.0]')
+    cmds.append('sigma[1,0,5.0]')
+    cmds.append('expar[-0.1,-1,0]')
+    cmds.append("RooVoigtian::voig(x,mz,width,sigma)")
+    cmds.append("RooExponential::exp(x,expar)")
+    cmds.append('nvoig[1,0,1000000]')
+    cmds.append('nexp[1,0,1000000]')
+    cmds.append("SUM::voigbg(nvoig*voig,nexp*exp)")
+    [w.factory(cmd) for cmd in cmds]
+    if fixw:
+        w.var('width').setConstant(kTRUE) if w.var('width') else None
+    return w.pdf('voigbg'), kTRUE
 
 # Simple gaussian
-if False:
-    minZ = '88.0'
-    maxZ = '94.0'
-    cmd = "RooGaussian::sum(x[%s,%s],mean[%s],sigma[1,0,5])"%(minZ,maxZ,'%s,%s,%s'%(mZ,minZ,maxZ))
-    print cmd
-    w.factory(cmd)
-    isExt = kFALSE
-
-# fancy shape with interference term, but no BG
-# TODO - RooRealSumPdf with amplitudes
-# see: http://root.cern.ch/root/html/tutorials/roofit/rf704_amplitudefit.C.html
-if False:
-    minZ='70.0'
-    maxZ='110.0'
-    A='1.0,0,1000000'
-    B='100.0,0,1000000'
-    C='10000.0,0,1000000'
-    # Prepare the Z lineshape
-    cmd = "EXPR::bw('A/(x*x)+B*(x*x-mean*mean)/((x*x-mean*mean)*(x*x-mean*mean)+width*width*mean*mean)+C*x*x/((x*x-mean*mean)*(x*x-mean*mean)+width*width*mean*mean)',x[{0},{1}],mean[91.18,{0},{1}],width[2.495,0.1,10],A[{2}],B[{3}],C[{4}])".format(minZ,maxZ,A,B,C)
-    w.factory(cmd)
-    # Prepare the Gaussian
-    cmd = "Gaussian::gaus(x,m_r[0.0],sigma[2.5,0.1,10])"
-    w.factory(cmd)
-    w.var('x').setBins(10000,'cache')
-    conv = 'FCONV::sum(x,bw,gaus)'
-    w.factory(conv)
-    w.var('m_r').setConstant(kTRUE) if w.var('m_r') else None
-    isExt = kFALSE
+def make_gaus(minZ,maxZ,mZ=mZ):
+    cmds = []
+    cmds.append('x[%s,%s]'%(minZ,maxZ))
+    cmds.append('mz[%s,%s,%s]'%(mZ,minZ,maxZ))
+    cmds.append('sigma[1.0,0,5.0]')
+    cmds.append("RooGaussian::gaus(x,mz,sigma)")
+    print cmds
+    [w.factory(cmd) for cmd in cmds]
+    return w.pdf('gaus'), kFALSE
 
 # fancy shape with interference term, but no BG - via AmplitudeFit
-if True:
-    minZ='70.0'
-    maxZ='110.0'
-    A='1000.0,0,1000000'
-    B='1000.0,0,1000000'
-    C='10000.0,0,1000000'
-    # Prepare the Z lineshape
+def make_bwfull(minZ,maxZ,isDouble=False,mZ=mZ):
+    """ isDouble enabled a double-gaussian resolution model """
     cmds = []
-    cmds.append( "expr::z_rad('1/(x^2)',x[%s,%s])"%(minZ,maxZ))
-    cmds.append( "expr::z_int('(x^2-mean^2)/((x^2-mean^2)^2+width^2*mean^2)',x,mean[91.18,%s,%s],width[2.495,0.1,10])"%(minZ,maxZ) )
-    cmds.append( "expr::z_rbw('x^2/((x^2-mean^2)^2+width^2*mean^2)',x,mean,width)" )
-    cmds.append( "A[%s]"%A )
-    cmds.append( "B[%s]"%B )
-    cmds.append( "C[%s]"%C )
-    cmds.append( "Gaussian::gaus(x,m_r[0.0],sigma[2.5,0.1,10])" )
+    # coefficients for the amplitudes
+    cmds.append("A[1,0,1000000]")
+    cmds.append("B[1,0,1000000]")
+    cmds.append("C[10000.0,0,1000000]")
+    # amplitudes
+    cmds.append('x[%s,%s]'%(minZ,maxZ))
+    cmds.append('mz[%s,%s,%s]'%(mZ,minZ,maxZ))
+    cmds.append('g[8,0,100]')
+    denom = '(x^2-mz^2)^2+x^4*g^2/mz^2'
+    cmds.append("expr::z_rbw('x^2/(%s)^2',x,mz,g)"%denom)
+    cmds.append("expr::z_int('(x^2-mz^2)/(%s)^2',x,mz,g)"%denom)
+    cmds.append("expr::z_rad('1/(x^2)',x)")
+    # gaussian resolution
+    cmds.append("m_r[0.0,0,10]")
+    cmds.append("sigma[2,0,10]")
+    cmds.append( "Gaussian::gaus(x,m_r,sigma)" )
     [w.factory(cmd) for cmd in cmds]
+    # any parameter adjustments
+    if False:
+        w.var('m_r').setConstant(kTRUE) if w.var('m_r') else None
     lshape = RooRealSumPdf('lshape','lshape',RooArgList(w.function('z_rad'),w.function('z_int'),w.function('z_rbw')),RooArgList(w.var('A'),w.var('B'),w.var('C')))
     getattr(w,'import')(lshape)
-    if True:  # double-gaussian
+    lgaus='gaus'
+    if isDouble:  # double-gaussian
         x = w.var('x')
-        m_r = w.var('m_r')
-        sigma_core = RooRealVar("sigma_core","sigma core",1) ;
-        gaussm_core = RooGaussModel("gaussm_core","core gauss",x,m_r,sigma_core) ;
-        sigma_tail = RooRealVar("sigma_tail","sigma tail",5) ;
-        gaussm_tail = RooGaussModel("gaussm_tail","tail gauss",x,m_r,sigma_tail) ;
+        mean_core = RooRealVar("mean_core","mean core",0,0,10) ;
+        sigma_core = RooRealVar("sigma_core","sigma core",1.5,0,10) ;
+        gaussm_core = RooGaussModel("gaussm_core","core gauss",x,mean_core,sigma_core) ;
+        mean_tail = RooRealVar("mean_tail","mean tail",0,0,10) ;
+        sigma_tail = RooRealVar("sigma_tail","sigma tail",3,0,10) ;
+        gaussm_tail = RooGaussModel("gaussm_tail","tail gauss",x,mean_tail,sigma_tail) ;
         frac_core = RooRealVar("frac_core","core fraction",0.85) ;
         gaussm = RooAddModel("gaussm","core+tail gauss", RooArgList(gaussm_core,gaussm_tail),RooArgList(frac_core)) ; 
         getattr(w,'import')(gaussm)
+        if True:
+            w.var('mean_core').setConstant(kTRUE) if w.var('mean_core') else None
+            w.var('mean_tail').setConstant(kTRUE) if w.var('mean_tail') else None
+        lgaus = 'gaussm'
     w.var('x').setBins(10000,'cache')
-    cmd = 'FCONV::sum(x,lshape,gaus)'
-    cmd = 'FCONV::sum(x,lshape,gaussm)'
+    cmd = 'FCONV::sum(x,lshape,%s)'%lgaus
     w.factory(cmd)
+    return w.pdf('sum'), kFALSE
+
+# Just clear bw with gaussian
+def make_bw(minZ,maxZ,mZ=mZ):
+    cmds = []
+    # amplitudes
+    cmds.append('x[%s,%s]'%(minZ,maxZ))
+    cmds.append('mz[%s,%s,%s]'%(mZ,minZ,maxZ))
+    cmds.append('g[2.495,0,100]')
+    cmds.append("EXPR::z_rbw('x^2/((x^2-mz^2)^2+x^2*g^2/mz^2)',x,mz,g)")
+    # gaussian resolution
+    cmds.append("m_r[0.0]")
+    cmds.append("sigma[1.5,0,100]")
+    cmds.append( "Gaussian::gaus(x,m_r,sigma)" )
+    [w.factory(cmd) for cmd in cmds]
+    # any parameter adjustments
     w.var('m_r').setConstant(kTRUE) if w.var('m_r') else None
-    isExt = kFALSE
-
-frange = (float(minZ),float(maxZ))
-
-# make a joint pdf for various signal regions
-w.factory("")
-w.defineSet('X','x')
-model = w.pdf('sum')
-nsig,nbg = w.var('nsig'),w.var('nbg')
-A,B,C = w.var('A'),w.var('B'),w.var('C')
-mean = w.var('mean')
-#mean.setConstant(kTRUE)
-x = w.var('x')
-chi2 = []
+    w.var('x').setBins(10000,'cache')
+    cmd = 'FCONV::sum(x,z_rbw,gaus)'
+    w.factory(cmd)
+    return w.pdf('sum'), kFALSE
 
 def PrintVariables():
-    model.Print('t')
+    #model.Print('t')
     vars = model.getVariables()
     vars.Print('v')
 
-def Fit(data):
-    # set some default event fractions based on the histogram
-    if nsig and nbg:
-        nsig.setVal(data.sumEntries())
-        nbg.setVal(0)
-    if C:
-        C.setVal(data.sumEntries())
+def Fit(data,isExt,bins,extras=False):
     # named ranges can be used in RF.Range in a comma-separated list
-    x.setRange('named_range',85,95)
-    r = model.fitTo(data,RF.PrintLevel(1),RF.Range(*frange),RF.Extended(isExt),RF.NumCPU(4),RF.Save())
+    x = w.var('x')
+    r = model.fitTo(data,RF.PrintLevel(1),RF.Extended(isExt),RF.NumCPU(4),RF.Save())
     frame = x.frame()
-    RooAbsData.plotOn(data,frame,RF.Name('dataZ'))
+    if data.ClassName()=='RooDataSet':
+        RooAbsData.plotOn( data,frame,RF.Name('dataZ'),RF.Binning(int((bins[1]-bins[0])/0.5)) )
+    else:
+        RooAbsData.plotOn(data,frame,RF.Name('dataZ'))
     model.plotOn(frame,RF.Name('modelZ'))
     ndf = r.floatParsFinal().getSize()
-    chi2.append(ndf)
-    chi2.append(frame.chiSquare(ndf))
+    chi2ndf = frame.chiSquare(ndf)
     # wildcards or comma-separated components are allowed:
     if isExt:
         model.plotOn(frame,RF.Components('exp*'),RF.LineStyle(kDashed))
-    if False:
+    if extras:
         model.plotOn(frame,RF.VisualizeError(r))
         model.paramOn(frame,data)
-    return (r,frame)
+    return (r,frame,chi2ndf,ndf)
 
-# getting data from histo
+def load_graph(hz,xmin,xmax,scale=1.0):
+    """ Load TGraph from a file. Note that we manually drop the points outside [xmin,xmax] range """
+    N = hz.GetN()
+    print 'Loading graph with',N,'entries'
+    v1 = hz.GetX()
+    v2 = hz.GetY()
+    ds1 = RooDataSet('ds1','ds1',RooArgSet(w.var('x')))
+    ds2 = RooDataSet('ds2','ds2',RooArgSet(w.var('x')))
+    nmax = min(N,opts.ndata); nmaxp10 = nmax if nmax>10 else 10
+    bar = SimpleProgressBar.SimpleProgressBar(10,nmax)
+    x = w.var('x')
+    for i in range(0,nmax):
+        if i%(int(nmaxp10/10))==0 and i>0:
+            print bar.show(i)
+        xv = func_SCALE(v1[i],scale)
+        if xmin < xv < xmax:
+            x.setVal(xv)
+            ds1.add(RooArgSet(x))
+    getattr(w,'import')(ds1,RF.Rename('ds1'))
+    getattr(w,'import')(ds2,RF.Rename('ds2'))
+    return ds1,ds2
+
+def load_histo(hz):
+    """ Load TH1 histogram from a file """
+    print 'Loading histogram with',hz.GetEntries(),'entries'
+    data = RooDataHist('data','Zmumu MC',RooArgList(w.var('x')),hz)
+    return data
+
+# getting data
 if True:
-    c = ROOT.TCanvas('c','c'); c.cd()
-    f = TFile(_fin,'r')
-    hz = f.Get(_hist)
-    hz.SetDirectory(0)
-    print hz.GetEntries()
+    # choose fit shape
+    if opts.gaus:
+        model,isExt = make_gaus(opts.min,opts.max)
+    elif opts.bw:
+        model,isExt = make_bw(opts.min,opts.max)
+    elif opts.bwfull:
+        model,isExt = make_bwfull(opts.min,opts.max,opts.double)
+    elif opts.voigbg:
+        model,isExt = make_voigbg(opts.min,opts.max)
+    else:
+        print 'Need at least one shape flag. Exiting...'
+        sys.exit(0)
+    # load the data
+    f = TFile(opts.root,'r')
+    hz = f.Get(opts.data)
+    cname = hz.ClassName()
+    if cname == 'TGraph':
+        data,crap = load_graph(hz,opts.min,opts.max,opts.scale)
+    elif re.search('TH',cname):
+        hz.SetDirectory(0)
+        data = load_histo(hz)
     f.Close()
-    data = RooDataHist('data','Zmumu MC',RooArgList(x),hz)
-    r,frame = Fit(data)
-    c.cd()
+    # set some default event fractions
+    if w.var('nsig') and w.var('nbg'):
+        w.var('nsig').setVal(data.sumEntries())
+        w.var('nbg').setVal(0)
+    if w.var('C'):
+        w.var('C').setVal(data.sumEntries())
+    # perform the fit
+    bins = (opts.min,opts.max)
+    r,frame,chi2ndf,ndf = Fit(data,isExt,bins)
+    # make the plots
+    c = ROOT.TCanvas('c','c'); c.cd()
     frame.Draw()
-    c.SaveAs('plot.png')
+    c.SaveAs('%s_fit.png'%opts.tag)
     PrintVariables()
-    mean=w.var('mean').getVal()
-    emean=w.var('mean').getError()
-    sigma=w.var('sigma').getVal()
-    print 'mW = %.3f +/- %.3f %.2f%% %.2f%%'%(mean,emean,(mean-fmZ)/fmZ*100.0,emean/fmZ*100.0)
-    if nbg and nsig:
-        print 'nsig = %d, nbg = %d'%(nsig.getVal(),nbg.getVal())
-    print 'CHI2 = %.2f, NDF = %d'%(chi2[1],chi2[0])
-
-if False:
-    data = model.generate(w.set('X'),2000)
-    r,frame = Fit(data)
-
-if False:
-    frame = x.frame()
-    model.plotOn(frame)
-    frame.Draw()
+    mz=w.var('mz').getVal()
+    emz=w.var('mz').getError()
+    print 'mZ = %.3f +/- %.3f'%(mz,emz)
+    print 'CHI2/NDF = %.2f, NDF = %d'%(chi2ndf,ndf)
