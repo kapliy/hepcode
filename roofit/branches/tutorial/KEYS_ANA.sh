48ae90d8a39961d3c69111aa0fe181fa73188329
antonk <antonk@639f0546-53f7-3a65-3376-bd91f4a7b4f4>
2011-05-02 23:15:19 +0000

    Major update to all functions, prepare for better systematic studies (varbins, ngroups etc), consolidation into fewer composite plots

--- roofit/branches/tutorial/KEYS_ANA.sh
+++ roofit/branches/tutorial/KEYS_ANA.sh
@@ -18,7 +18,7 @@ if [ "1" -eq "1" ]; then
     for reg in AA BB CC; do
 	subfile=${lbl_mc}_${lbl_mc}.root;
 	gr=${subfile}/dg/dg/st_z_final/ntuple${ntlbl}
-	tag="ZMC_${rfile}_${lbl_mc}_${tt}_${reg}"
+	tag="ZMC_`basename ${rfile}`_${lbl_mc}_${tt}_${reg}"
 	xtra=""
 	if [ "${func}" == "gaus" ]; then xtra="--min 88 --max 94"; fi
 	./zpeak.py -b --root ${rfile} --res 3 --${func} --region ${reg} --data ${gr} --ndata ${nmc} -t ${tag} ${xtra}
@@ -30,13 +30,13 @@ if [ "1" -eq "1" ]; then
     for reg in AA BB CC; do
 	subfile=${lbl_data}_${lbl_data}.root;
 	gr=${subfile}/dg/dg/st_z_final/ntuple${ntlbl}
-	RZ=ZMC_${rfile}_${lbl_mc}_${tt}_${reg}${sfx}
+	RZ=ZMC_`basename ${rfile}`_${lbl_mc}_${tt}_${reg}${sfx}
 	mz0=`tail -n2 ${RZ} | head -n1 | cut -d ' ' -f 3`
-	RKS=${rfile}_data_${tt}_data_${reg}_10000_ks${sfx}
-	RCHI=${rfile}_data_${tt}_data_${reg}_10000${sfx}
+	RKS=`basename ${rfile}`_data_${tt}_data_${reg}_10000_ks${sfx}
+	RCHI=`basename ${rfile}`_data_${tt}_data_${reg}_10000${sfx}
 	R=`cut -d ' ' -f 13 ${RKS}`
 	eR=`cut -d ' ' -f 5 ${RCHI}`
-	tag="ZDATA_${rfile}_${lbl_data}_${tt}_${reg}"
+	tag="ZDATA_`basename ${rfile}`_${lbl_data}_${tt}_${reg}"
 	echo "Printing contents of input files for region ${reg}:"
 	echo ${RZ} ${RKS} ${RCHI}
 	cat ${RZ} ${RKS} ${RCHI} /dev/null
--- roofit/branches/tutorial/RUN_PBS.sh
+++ roofit/branches/tutorial/RUN_PBS.sh
@@ -3,24 +3,23 @@
 ROOTDIR=$PWD
 i=0
 
-xtra="--kolmogorov --shift --kluit"
-oks[0]=" "
-oks[1]="--kolmogorov"
-
 oft[0]=" "
-oft[1]="--shift"
+#oft[1]="--shift"
 
 okl[0]=" "
 okl[1]="--kluit"
+okl[2]="--akluit"
 
-fname=ROOT/root_all_apr26data2011BD3.root
-regs="AA BB CC Bcc Baa FWC FWA MWC MWA"
-folders="data_2010repro  data_20110331 data_20110425B  data_20110425D"
-tts="cmb id ms exms"
-for fold in $folders; do
-    for tt in $tts; do
-	for reg in $regs; do
-	    for iok in "${oks[@]}"; do
+xtra="--template"
+
+fnames="ROOT/root_all_0428.root"
+regs="AA BB CC Bcc Baa FWC FWA MWC MWA FWC0 FWC1 FWC2 FWC3 FWC4 FWA0 FWA1 FWA2 FWA3 FWA4"
+folders="data data_20110425"
+tts="cmb id exms"
+for fname in $fnames; do
+    for fold in $folders; do
+	for tt in $tts; do
+	    for reg in $regs; do
 		for ift in "${oft[@]}"; do
 		    for ikl in "${okl[@]}"; do
 			J=JOB$i.sh
@@ -30,9 +29,9 @@ for fold in $folders; do
 			echo "#PBS -l cput=24:00:00" >> $J
 			echo "#PBS -l walltime=24:00:00" >> $J
 			echo "source ~/.bashrc" >> $J
-			echo "anaquick" >> $J
+			echo "anaquick2" >> $J
 			echo "cd ${ROOTDIR}" >> $J
-			echo ./KEYS_CMB.sh ${fname} ${fold} ${tt} data ${reg} $iok $ift $ikl >> $J
+			echo ./KEYS_CMB.sh ${fname} ${fold} ${tt} data ${reg} $ift $ikl ${template} >> $J
 			qsub -N keys${i} -o LOG${i}.out -e LOG${i}.err ${J}
 			((i++))
 		    done
