c3d85ba3110480b7c17d9e0cf22c2ca063076b3b
antonk <antonk@639f0546-53f7-3a65-3376-bd91f4a7b4f4>
2011-05-03 06:05:48 +0000

    Added python scripting to summarize absolute scales, too

--- roofit/branches/tutorial/KEYS_ANA.sh
+++ roofit/branches/tutorial/KEYS_ANA.sh
@@ -4,60 +4,62 @@ rfile=$1
 lbl_data=$2
 lbl_mc=$3
 tt=$4
+reg=$5
+pfx=$6
 
 ntlbl="_${tt}"
 if [ "$tt" == "cmb" ]; then ntlbl=""; fi;
 
 nmc=100000
 func=egge   #gaus
+res=3
 
 sfx="_results.rtxt"
 
 # truth on full dataset
 if [ "1" -eq "1" ]; then
-    for reg in AA BB CC; do
-	subfile=${lbl_mc}_${lbl_mc}.root;
-	gr=${subfile}/dg/dg/st_z_final/ntuple${ntlbl}
-	tag="ZMC_`basename ${rfile}`_${lbl_mc}_${tt}_${reg}"
-	xtra=""
-	if [ "${func}" == "gaus" ]; then xtra="--min 88 --max 94"; fi
-	./zpeak.py -b --root ${rfile} --res 3 --${func} --region ${reg} --data ${gr} --ndata ${nmc} -t ${tag} ${xtra}
-    done
+    subfile=${lbl_mc}_${lbl_mc}.root;
+    gr=${subfile}/dg/dg/st_z_final/ntuple${ntlbl}
+    tag="ZMC_${pfx}`basename ${rfile}`_${lbl_mc}_${tt}_${reg}"
+    xtra=""
+    if [ "${res}" == "1" ]; then xtra="--min 80 --max 100"; fi
+    if [ "${func}" == "gaus" ]; then xtra="--min 88 --max 94"; fi
+    ./zpeak.py -b --root ${rfile} --res ${res} --${func} --region ${reg} --data ${gr} --ndata ${nmc} -t ${tag} ${xtra}
 fi;
 
 # data, using MC fitted value from truth and relative scale (R0/eR0) from keysfit.py
 if [ "1" -eq "1" ]; then
-    for reg in AA BB CC; do
-	subfile=${lbl_data}_${lbl_data}.root;
-	gr=${subfile}/dg/dg/st_z_final/ntuple${ntlbl}
-	RZ=ZMC_`basename ${rfile}`_${lbl_mc}_${tt}_${reg}${sfx}
-	mz0=`tail -n2 ${RZ} | head -n1 | cut -d ' ' -f 3`
-	RKS=`basename ${rfile}`_data_${tt}_data_${reg}_10000_ks${sfx}
-	RCHI=`basename ${rfile}`_data_${tt}_data_${reg}_10000${sfx}
-	R=`cut -d ' ' -f 13 ${RKS}`
-	eR=`cut -d ' ' -f 5 ${RCHI}`
-	tag="ZDATA_`basename ${rfile}`_${lbl_data}_${tt}_${reg}"
-	echo "Printing contents of input files for region ${reg}:"
-	echo ${RZ} ${RKS} ${RCHI}
-	cat ${RZ} ${RKS} ${RCHI} /dev/null
-	if [ -z "${R}" -o -z "${eR}" -o -z "${mz0}" ]; then
-	    echo "Skipping region ${reg} - missing some inputs"
-	    echo "R=|${R}| eR=|${eR}| mz0=|${mz0}|"
-	    continue
-	fi;
-	./zpeak.py -b --root ${rfile} --res 3 --${func} --region ${reg} --mz0 ${mz0} --R ${R} --eR ${eR} --data ${gr} --ndata 10000 -t ${tag}
-    done;
+    subfile=${lbl_data}_${lbl_data}.root;
+    gr=${subfile}/dg/dg/st_z_final/ntuple${ntlbl}
+    RZ=ZMC_${pfx}`basename ${rfile}`_${lbl_mc}_${tt}_${reg}${sfx}
+    mz0=`tail -n2 ${RZ} | head -n1 | cut -d ' ' -f 3`
+    RTXT=${pfx}`basename ${rfile}`_${lbl_data}_${tt}_data_${reg}${sfx}
+    R=`head -n1 ${RTXT} | cut -d ' ' -f 14`
+    eR=`tail -n1 ${RTXT} | cut -d ' ' -f 6`
+    tag="ZDATA_${pfx}`basename ${rfile}`_${lbl_data}_${tt}_${reg}"
+    echo "Printing contents of input files for region ${reg}:"
+    echo ${RZ} ${RTXT}
+    cat ${RZ} ${RTXT} /dev/null
+    if [ -z "${R}" -o -z "${eR}" -o -z "${mz0}" ]; then
+	echo "Skipping region ${reg} - missing some inputs"
+	echo "R=|${R}| eR=|${eR}| mz0=|${mz0}|"
+    else
+	xtra=""
+	if [ "${res}" == "1" ]; then xtra="--min 80 --max 100"; fi
+	if [ "${func}" == "gaus" ]; then xtra="--min 88 --max 94"; fi
+	./zpeak.py -b --root ${rfile} --res ${res} --${func} --region ${reg} --mz0 ${mz0} --R ${R} --eR ${eR} --data ${gr} ${xtra} -t ${tag}
+    fi;
 fi;
 
-#BARREL-ENDCAP CROSS (CROSS-CHECK):
+#BARREL-ENDCAP CROSS-CHECK:
 if [ "0" -eq "1" ]; then
     func=egge
     reg=BA
-    ./zpeak.py -b --root ${rfile} --res 3 --${func} --mz0 90.963 --kp 1.00114 --ekp 0.00191 --data data_data.root/dg/dg/st_z_final/$reg/graph_Z_m_eta --ndata 10000 -t data_${reg}_${func}
+    ./zpeak.py -b --root ${rfile} --res ${res} --${func} --mz0 90.963 --kp 1.00114 --ekp 0.00191 --data data_data.root/dg/dg/st_z_final/$reg/graph_Z_m_eta --ndata 10000 -t data_${reg}_${func}
     reg=BC
-    ./zpeak.py -b --root ${rfile} --res 3 --${func} --mz0 90.950 --kp 1.00114 --ekp 0.00191 --data data_data.root/dg/dg/st_z_final/$reg/graph_Z_m_eta --ndata 10000 -t data_${reg}_${func}
+    ./zpeak.py -b --root ${rfile} --res ${res} --${func} --mz0 90.950 --kp 1.00114 --ekp 0.00191 --data data_data.root/dg/dg/st_z_final/$reg/graph_Z_m_eta --ndata 10000 -t data_${reg}_${func}
     reg=AB
-    ./zpeak.py -b --root ${rfile} --res 3 --${func} --mz0 90.914 --km 0.996 --ekm 0.002 --data data_data.root/dg/dg/st_z_final/$reg/graph_Z_m_eta --ndata 10000 -t data_${reg}_${func}
+    ./zpeak.py -b --root ${rfile} --res ${res} --${func} --mz0 90.914 --km 0.996 --ekm 0.002 --data data_data.root/dg/dg/st_z_final/$reg/graph_Z_m_eta --ndata 10000 -t data_${reg}_${func}
     reg=CB
-    ./zpeak.py -b --root ${rfile} --res 3 --${func} --mz0 90.902 --km 0.996 --ekm 0.002 --data data_data.root/dg/dg/st_z_final/$reg/graph_Z_m_eta --ndata 10000 -t data_${reg}_${func}
+    ./zpeak.py -b --root ${rfile} --res ${res} --${func} --mz0 90.902 --km 0.996 --ekm 0.002 --data data_data.root/dg/dg/st_z_final/$reg/graph_Z_m_eta --ndata 10000 -t data_${reg}_${func}
 fi
--- roofit/branches/tutorial/RUN_ANA.sh
+++ roofit/branches/tutorial/RUN_ANA.sh
@@ -1,16 +1,44 @@
 #!/bin/bash
 
+ROOTDIR=$PWD
 i=0
 
-./KEYS_ANA.sh root_all_0331.root data mc_zmumu cmb &> LOG.ANA.${i} &
-((i++))
-./KEYS_ANA.sh root_all_0331.root data mc_zmumu id &> LOG.ANA.${i} &
-((i++))
-./KEYS_ANA.sh root_all_0331.root data mc_zmumu ms &> LOG.ANA.${i} &
-((i++))
-./KEYS_ANA.sh root_all_0331.root data mc_zmumu exms &> LOG.ANA.${i} &
-((i++))
-wait
+oft[0]=" "
+#oft[1]="--shift"
+
+okl[0]=""
+okl[1]="KLU_"
+okl[2]="AKLU_"
+
+fnames="ROOT/root_all_0428.root"
+regs="AA BB CC Bcc Baa FWC FWA MWC MWA" # "FWC0 FWC1 FWC2 FWC3 FWA0 FWA1 FWA2 FWA3"
+folders[0]="data mc_zmumu"
+folders[1]="data_20110425 mc_20110425zmumu"
+tts="cmb id exms"
+
+for fname in $fnames; do
+    for fold in "${folders[@]}"; do
+ 	for tt in $tts; do
+	    for reg in $regs; do
+		for ikl in "${okl[@]}"; do
+		    J=JOB$i.sh
+		    rm -f $J; touch $J; chmod +x $J
+		    echo "#!/bin/bash" >> $J
+		    echo "#PBS -q uct3" >> $J
+		    echo "#PBS -l cput=24:00:00" >> $J
+		    echo "#PBS -l walltime=24:00:00" >> $J
+		    echo "source ~/.bashrc" >> $J
+		    echo "anaquick2" >> $J
+		    echo "cd ${ROOTDIR}" >> $J
+		    echo ./KEYS_ANA.sh ${fname} ${fold} ${tt} ${reg} ${ikl} >> $J
+		    qsub -N zfits${i} -o LOG.zfits.${i}.out -e LOG.zfits.${i}.err ${J}
+		    ((i++))
+		done
+	    done
+	done
+    done
+done
 echo "Done up to ${i}"
 
-echo "Completely done!"
+#./KEYS_ANA.sh ROOT/root_all_0428.root data mc_zmumu cmb BB ''
+#./KEYS_ANA.sh ROOT/root_all_0428.root data_20110425 mc_20110425zmumu cmb BB 'KLU_'
