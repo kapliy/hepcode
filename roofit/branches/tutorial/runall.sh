169b4b8e946abaebc90a87cf8afecc4ec451c51b
antonk <kapliy@gmail.com>
2011-05-24 17:13:46 +0000

    Small executable to run all-detector z fits for final scale validation

---
+++ roofit/branches/tutorial/runall.sh
@@ -0,0 +1,11 @@
+fnames="ROOT/root_all_0516_2010clos.root"
+fnames="ROOT/root_all_0522_dataMCclosure.root"
+funcs="gaus"
+folders="data data_00 data_33 mc_zmumu mc_zmumu_00 mc_zmumuO mc_zmumuO_00 mc_zmumuO_22"
+for fname in $fnames; do
+    for func in $funcs; do
+	for fold in ${folders}; do
+	    ./zpeak.py -b --root ${fname} --res 3 --${func} --region ALL --data ${fold}_${fold}.root/dg/dg/st_z_final/ntuple --ndata 100000 -t ALL_${fold}_${func}
+	done
+    done
+done
