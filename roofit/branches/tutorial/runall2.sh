9a652d70bff934cb0997093b27cb7edfb65c5446
antonk <antonk@639f0546-53f7-3a65-3376-bd91f4a7b4f4>
2011-05-25 05:10:39 +0000

    Updated scripts to study 1/pt KS improvement due to application of absolute scales in data - at TrigFTKAna level

--- roofit/branches/tutorial/runall2.sh
+++ roofit/branches/tutorial/runall2.sh
@@ -1,14 +1,14 @@
 fnames="ROOT/root_all_0522_dataMCclosure.root"
 funcs="gaus"
-regs="ALL AA BB CC"
+regs="AB BA CB BC BB AA CC"
 tts="cmb id exms"
 
 i=0
-folders[((i++))]="data mc_zmumu"
-#folders[((i++))]="data mc_zmumu_00"
+#folders[((i++))]="data mc_zmumu"
 folders[((i++))]="data mc_zmumuO"
 folders[((i++))]="data mc_zmumuO_00"
 folders[((i++))]="data mc_zmumuO_22"
+folders[((i++))]="data mc_zmumuO_33"
 
 for tt in $tts; do
     ntlbl="_${tt}"
@@ -19,8 +19,9 @@ for tt in $tts; do
 		for fold in "${folders[@]}"; do
 		    data=`echo ${fold} | cut -d ' ' -f 1`
 		    mc=`echo ${fold} | cut -d ' ' -f 2`
-		    ./zkolmogorov.py -b --root ${fname} --region ${reg} --data ${data}_${data}.root/dg/dg/st_z_final/ntuple${ntlbl} --mc ${mc}_${mc}.root/dg/dg/st_z_final/ntuple${ntlbl} --min 70 --max 110 -t KS_${tt}_${reg}_${data}_${mc}
+		    ./zkolmogorov.py -b --root ${fname} --region ${reg} --data ${data}_${data}.root/dg/dg/st_z_final/ntuple${ntlbl} --mc ${mc}_${mc}.root/dg/dg/st_z_final/ntuple${ntlbl} --min 70 --max 110 -t KS_${tt}_${reg}_${data}_${mc} &
 		done
+		wait
 	    done
 	done
     done
