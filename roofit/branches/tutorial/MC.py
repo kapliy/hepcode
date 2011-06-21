51cfc9925f898d0c1886bd6f352fd6dbc50b632a
antonk <antonk@639f0546-53f7-3a65-3376-bd91f4a7b4f4>
2011-06-21 16:57:02 +0000

    Updated all plots to SuPlot. Updated mu15x QCD scale to 0.45

--- roofit/branches/tutorial/MC.py
+++ roofit/branches/tutorial/MC.py
@@ -2,9 +2,6 @@
 import sys,os,re
 import ROOT
 
-POS=0
-NEG=1
-
 seen_samples = []
 def ScaleToLumi(h,name,lumi,qcdscale,nevts=None):
     mrun = mc09.match_sample(name)
@@ -15,6 +12,7 @@ def ScaleToLumi(h,name,lumi,qcdscale,nevts=None):
         if sample not in seen_samples:
             print 'MC %s: \t xsec=%.1f nb \t nevts=%d/%d'%(sample,xsec,nevents,mrun.nevents)
             seen_samples.append(sample)
+        h.Sumw2()
         h.Scale(1.0/nevents*lumi*xsec)
         if qcdscale!='AUTO' and sample in ['mc_J%d'%z for z in range(10)]:
             h.Scale(float(qcdscale))
@@ -96,10 +94,11 @@ class MC09_samples:
             return runs[0]
         return None
     def match_sample(s,path):
-        path_stripped = re.sub('mc_','',path)
-        runs = [a for a in s.runs if re.search(str(a.sample),str(path))]
+        runs = [a for a in s.runs if str(a.sample)==str(path)]
+        #runs = [a for a in s.runs if re.search(str(a.sample),str(path))]
         if len(runs)==1:
             return runs[0]
+        assert False,'Failed to find sample %s'%path
         return None
     def get_xsec(s,path):
         return s.match_run(path).xsec
@@ -110,53 +109,90 @@ class MC09_samples:
 
 mc09 = MC09_samples()
 
-mc09.append(MCR(106044,'mc_wmunu','*',8894.06*1.17/1000,1.000000,6993798))
-mc09.append(MCR(106022,'mc_wtaunu','*',8916.33*1.17/1000,0.87867,999874))
-mc09.append(MCR(106047,'mc_zmumu','*',851.011*1.15/1000,1.000000,4998410))
-mc09.append(MCR(106052,'mc_ztautau','*',856.967*1.15/1000,1.000000,1998598))
-mc09.append(MCR(105861,'mc_ttbar','*',1.4580E-1,0.54301,199838))
-# these are incorrect:
-mc09.append(MCR(113204,'mc_J0','*',9.6139E+06,1.0,500000))
-mc09.append(MCR(113205,'mc_J1','*',7.4366E+05,1.0,500000))
-mc09.append(MCR(113206,'mc_J2','*',4.4307E+04,1.0,500000))
-mc09.append(MCR(113207,'mc_J3','*',2.3576E+03,1.0,500000))
-mc09.append(MCR(113208,'mc_J4','*',9.4236E+01,1.0,500000))
-mc09.append(MCR(113209,'mc_J5','*',2.5813E+00,1.0,500000))
-mc09.append(MCR(113210,'mc_J6','*',3.9439E-02,1.0,500000))
-#from here: http://cdsweb.cern.ch/record/1282370/files/ATL-CAL-PROC-2010-001.pdf?version=3
-mc09.append(MCR(108405,'mc_bbmu15x','*',7.39E+04/1000,1.0,500000)) 
-mc09.append(MCR(106059,'mc_ccmu15x','*',2.84E+04/1000,1.0,500000))
-
-mc09.append(MCR(106044,'mc2_wmunu','*',8894.06*1.17/1000,1.000000,6993798))
-mc09.append(MCR(106022,'mc2_wtaunu','*',8916.33*1.17/1000,0.87867,999874))
-mc09.append(MCR(106047,'mc2_zmumu','*',851.011*1.15/1000,1.000000,4998410))
-mc09.append(MCR(106052,'mc2_ztautau','*',856.967*1.15/1000,1.000000,1998598))
-mc09.append(MCR(105861,'mc2_ttbar','*',1.4580E-1,0.54301,199838))
-# these are incorrect:
-mc09.append(MCR(113204,'mc2_J0','*',9.6139E+06,1.0,500000))
-mc09.append(MCR(113205,'mc2_J1','*',7.4366E+05,1.0,500000))
-mc09.append(MCR(113206,'mc2_J2','*',4.4307E+04,1.0,500000))
-mc09.append(MCR(113207,'mc2_J3','*',2.3576E+03,1.0,500000))
-mc09.append(MCR(113208,'mc2_J4','*',9.4236E+01,1.0,500000))
-mc09.append(MCR(113209,'mc2_J5','*',2.5813E+00,1.0,500000))
-mc09.append(MCR(113210,'mc2_J6','*',3.9439E-02,1.0,500000))
-#from here: http://cdsweb.cern.ch/record/1282370/files/ATL-CAL-PROC-2010-001.pdf?version=3
-mc09.append(MCR(108405,'mc2_bbmu15x','*',7.39E+04/1000,1.0,500000)) 
-mc09.append(MCR(106059,'mc2_ccmu15x','*',2.84E+04/1000,1.0,500000))
-
-
-if False: # mc09
+if False: # from Peter
     mc09.append(MCR(106044,'mc_wmunu','*',10.461,1.000000,6993798))
-    mc09.append(MCR(106022,'mc_wtaunu','*',10.461,0.873,999874))
-    mc09.append(MCR(106047,'mc_zmumu','*',0.989,1.000000,4998410))
-    mc09.append(MCR(106052,'mc_ztautau','*',0.989,1.000000,1998598))
-    mc09.append(MCR(105861,'mc_ttbar','*',0.165,0.5426,199838))
-    mc09.append(MCR(109276,'mc_J0','*',9.8605E+06,8.5572E-05,500000))
-    mc09.append(MCR(109277,'mc_J1','*',6.7804E+05,1.2111E-03,500000))
-    mc09.append(MCR(109278,'mc_J2','*',4.0968E+04,5.4310E-03,500000))
-    mc09.append(MCR(109279,'mc_J3','*',2.1929E+03,1.3051E-02,500000))
-    mc09.append(MCR(109280,'mc_J4','*',8.7665E+01,2.2357E-02,500000))
-    mc09.append(MCR(109281,'mc_J5','*',2.3488E+00,3.0230E-02,500000))
+    mc09.append(MCR(106022,'mc_wtaunu','*',8916.33*1.17/1000,0.87867,999874))
+    mc09.append(MCR(106047,'mc_zmumu','*',851.011*1.15/1000,1.000000,4998410))
+    mc09.append(MCR(106052,'mc_ztautau','*',856.967*1.15/1000,1.000000,1998598))
+    mc09.append(MCR(105861,'mc_ttbar','*',0.1646,5.4258E-01,199838))
+    # these are incorrect:
+    mc09.append(MCR(113204,'mc_J0','*',9.6139E+06,1.0,500000))
+    mc09.append(MCR(113205,'mc_J1','*',7.4366E+05,1.0,500000))
+    mc09.append(MCR(113206,'mc_J2','*',4.4307E+04,1.0,500000))
+    mc09.append(MCR(113207,'mc_J3','*',2.3576E+03,1.0,500000))
+    mc09.append(MCR(113208,'mc_J4','*',9.4236E+01,1.0,500000))
+    mc09.append(MCR(113209,'mc_J5','*',2.5813E+00,1.0,500000))
+    mc09.append(MCR(113210,'mc_J6','*',3.9439E-02,1.0,500000))
+    #from here: http://cdsweb.cern.ch/record/1282370/files/ATL-CAL-PROC-2010-001.pdf?version=3
+    mc09.append(MCR(108405,'mc_bbmu15x','*',7.39E+04/1000,1.0,500000)) 
+    mc09.append(MCR(106059,'mc_ccmu15x','*',2.84E+04/1000,1.0,500000))
+    mc09.append(MCR(106044,'mc_wminmunu','*',3.9607,1.000000,6993798))
+    mc09.append(MCR(106044,'mc_wplusmunu','*',5.8414,1.000000,6993798))
+
+if True: # before peter update
+    mc09.append(MCR(106044,'mc_wmunu','*',8894.06*1.17/1000,1.000000,6993798))
+    mc09.append(MCR(106022,'mc_wtaunu','*',8916.33*1.17/1000,0.87867,999874))
+    mc09.append(MCR(106047,'mc_zmumu','*',851.011*1.15/1000,1.000000,4998410))
+    mc09.append(MCR(106052,'mc_ztautau','*',856.967*1.15/1000,1.000000,1998598))
+    mc09.append(MCR(105861,'mc_ttbar','*',1.4580E-1,0.54301,199838))
+    # these are incorrect:
+    mc09.append(MCR(113204,'mc_J0','*',9.6139E+06,1.0,500000))
+    mc09.append(MCR(113205,'mc_J1','*',7.4366E+05,1.0,500000))
+    mc09.append(MCR(113206,'mc_J2','*',4.4307E+04,1.0,500000))
+    mc09.append(MCR(113207,'mc_J3','*',2.3576E+03,1.0,500000))
+    mc09.append(MCR(113208,'mc_J4','*',9.4236E+01,1.0,500000))
+    mc09.append(MCR(113209,'mc_J5','*',2.5813E+00,1.0,500000))
+    mc09.append(MCR(113210,'mc_J6','*',3.9439E-02,1.0,500000))
+    mc09.append(MCR(108405,'mc_bbmu15x','*',7.39E+04/1000,1.0,500000)) 
+    mc09.append(MCR(106059,'mc_ccmu15x','*',2.84E+04/1000,1.0,500000))
+
+# W/Z + jets samples:
+# http://cdsweb.cern.ch/record/1298803/files/ATL-COM-PHYS-2010-836.pdf
+mc09.append(MCR(105200,'mc_jimmy_ttbar','*',80.201/1000,1.11,500000))
+#wmunu
+mc09.append(MCR(107690,'mc_jimmy_wmunu_np0','*',6935.4/1000,1.22,500000))
+mc09.append(MCR(107691,'mc_jimmy_wmunu_np1','*',1281.2/1000,1.22,500000))
+mc09.append(MCR(107692,'mc_jimmy_wmunu_np2','*',375.3/1000,1.22,500000))
+mc09.append(MCR(107693,'mc_jimmy_wmunu_np3','*',101.1/1000,1.22,500000))
+mc09.append(MCR(107694,'mc_jimmy_wmunu_np4','*',25.7/1000,1.22,500000))
+mc09.append(MCR(107695,'mc_jimmy_wmunu_np5','*',7.0/1000,1.22,500000))
+#wtaunu
+mc09.append(MCR(107700,'mc_jimmy_wtaunu_np0','*',6835.8/1000,1.22,500000))
+mc09.append(MCR(107701,'mc_jimmy_wtaunu_np1','*',1276.8/1000,1.22,500000))
+mc09.append(MCR(107702,'mc_jimmy_wtaunu_np2','*',376.6/1000,1.22,500000))
+mc09.append(MCR(107703,'mc_jimmy_wtaunu_np3','*',100.8/1000,1.22,500000))
+mc09.append(MCR(107704,'mc_jimmy_wtaunu_np4','*',25.7/1000,1.22,500000))
+mc09.append(MCR(107705,'mc_jimmy_wtaunu_np5','*',7.0/1000,1.22,500000))
+#zmumu
+mc09.append(MCR(107660,'mc_jimmy_zmumu_np0','*',657.7/1000,1.22,500000))
+mc09.append(MCR(107661,'mc_jimmy_zmumu_np1','*',132.8/1000,1.22,500000))
+mc09.append(MCR(107662,'mc_jimmy_zmumu_np2','*',39.6/1000,1.22,500000))
+mc09.append(MCR(107663,'mc_jimmy_zmumu_np3','*',11.1/1000,1.22,500000))
+mc09.append(MCR(107664,'mc_jimmy_zmumu_np4','*',2.8/1000,1.22,500000))
+mc09.append(MCR(107665,'mc_jimmy_zmumu_np5','*',0.8/1000,1.22,500000))
+#ztautau
+mc09.append(MCR(107670,'mc_jimmy_ztautau_np0','*',657.4/1000,1.22,500000))
+mc09.append(MCR(107671,'mc_jimmy_ztautau_np1','*',133.0/1000,1.22,500000))
+mc09.append(MCR(107672,'mc_jimmy_ztautau_np2','*',40.4/1000,1.22,500000))
+mc09.append(MCR(107673,'mc_jimmy_ztautau_np3','*',11.0/1000,1.22,500000))
+mc09.append(MCR(107674,'mc_jimmy_ztautau_np4','*',2.9/1000,1.22,500000))
+mc09.append(MCR(107675,'mc_jimmy_ztautau_np5','*',0.7/1000,1.22,500000))
+#WW
+mc09.append(MCR(107100,'mc_jimmy_ww_np0','*',2.049/1000,1.21,500000))
+mc09.append(MCR(107101,'mc_jimmy_ww_np1','*',0.987/1000,1.21,500000))
+mc09.append(MCR(107102,'mc_jimmy_ww_np2','*',0.441/1000,1.21,500000))
+mc09.append(MCR(107103,'mc_jimmy_ww_np3','*',0.178/1000,1.21,500000))
+#WZ
+mc09.append(MCR(107104,'mc_jimmy_wz_np0','*',0.664/1000,1.21,500000))
+mc09.append(MCR(107105,'mc_jimmy_wz_np1','*',0.399/1000,1.21,500000))
+mc09.append(MCR(107106,'mc_jimmy_wz_np2','*',0.221/1000,1.21,500000))
+mc09.append(MCR(107107,'mc_jimmy_wz_np3','*',0.093/1000,1.21,500000))
+#ZZ
+mc09.append(MCR(107108,'mc_jimmy_zz_np0','*',0.494/1000,1.21,500000))
+mc09.append(MCR(107109,'mc_jimmy_zz_np1','*',0.225/1000,1.21,500000))
+mc09.append(MCR(107110,'mc_jimmy_zz_np2','*',0.088/1000,1.21,500000))
+mc09.append(MCR(107111,'mc_jimmy_zz_np3','*',0.028/1000,1.21,500000))
+
 
 if False:
     print 'Registered',mc09.nruns(),'runs:'
@@ -192,6 +228,24 @@ def SetStyle(styleMacro=''):
         ROOT.gROOT.LoadMacro(styleMacro);
         ROOT.SetAtlasStyle()
 
+def MakeLegend(label='Data and MC'):
+    l = ROOT.TLegend(0.55,0.70,0.88,0.88,label,"brNDC")
+    l.SetFillStyle(1001)
+    l.SetFillColor(10)
+    return l
+
+def is_wmunu(iname):
+    return any( [re.search(v,iname) for v in ('wmunu','wminmunu','wplusmunu')] )
+
+def is_qcd(iname):
+    return any( [re.search(v,iname) for v in ('mc_bbmu15x','mc_ccmu15x','mc_J')] )
+
+def WSum(hplus,hminus,hname):
+    """ Just a sum of two histograms """
+    h = hplus.Clone(hname)
+    h.Add(hminus)
+    return h
+
 def WRatio(hplus,hminus,title,label='ratio'):
     """ Ratio of W+ to W- workhorse function
     Alternatively, can be used to calculate efficiency
@@ -215,4 +269,3 @@ def canvas():
     c = ROOT.TCanvas("c1","c1",200,10,800,500)
     c.cd()
     return c
-
---
+++ roofit/branches/tutorial/SuCanvas.py
@@ -0,0 +1,252 @@
+#!/usr/bin/env python
+import re
+import ROOT
+
+class SuCanvas:
+  """ Jordan's canvas class for plotting """
+
+  def FixupHisto(s,h):
+    h.GetXaxis().SetTitleOffset( s.getXtitleOffset() );
+    h.GetYaxis().SetTitleOffset( s.getYtitleOffset() );
+    h.GetXaxis().SetLabelSize( s.getLabelSize() );
+    h.GetYaxis().SetLabelSize( s.getLabelSize() );
+    h.GetXaxis().SetTitleSize( s.getAxisTitleSize() );
+    h.GetYaxis().SetTitleSize( s.getAxisTitleSize() );
+    h.GetYaxis().CenterTitle();
+    h.GetXaxis().CenterTitle();
+    return h
+
+  def drawRatio(s,ratio):
+    """ Draws a ratio histogram """
+    ratio.SetFillColor( ROOT.kBlack );
+    ratio.SetFillStyle( 3154 );
+    ratio.SetMarkerStyle( 2 );
+    ratio.Draw("AP E2 same");
+                    
+  def drawRefLine(s,refLine,xtitle=None):
+    """ Draws a reference line """
+    for ibin in range (1,refLine.GetNbinsX()+1):
+      refLine.SetBinContent( ibin , 1. )
+    refLine.SetLineColor( ROOT.kAzure-7 );
+    refLine.SetMinimum( 0.7 )
+    refLine.SetMaximum( 1.3 )
+    refLine.GetXaxis().SetLabelSize( s.getRatioLabelSize() );
+    refLine.GetYaxis().SetLabelSize( s.getRatioLabelSize() );
+    refLine.GetXaxis().SetTitleSize( s.getRatioAxisTitleSize() );
+    refLine.GetYaxis().SetTitleSize( s.getRatioAxisTitleSize() );
+    refLine.GetXaxis().SetTitleOffset( s.getRatioXtitleOffset() );
+    refLine.GetYaxis().SetTitleOffset( s.getRatioYtitleOffset() );
+    refLine.GetYaxis().CenterTitle();
+    refLine.GetXaxis().CenterTitle();
+    refLine.GetYaxis().SetNdivisions( 406 );
+    if xtitle:
+      refLine.GetXaxis().SetTitle( xtitle );
+    refLine.GetYaxis().SetTitle( "data / BG" );
+    refLine.SetLineWidth( 3 );
+    refLine.SetLineStyle( ROOT.kDashed );
+    refLine.Draw( "hist" );
+
+  def plotStackHisto(s,stack,data,leg=None):
+    """ Wrapper to make a complete plot of stack and data overlayed """
+    s.buildRatio();
+    s.cd_plotPad();
+    # mc
+    stack.Draw("HIST")
+    s.FixupHisto(stack)
+    stack.SetMinimum(0.1)
+    maximum = max((data.GetMaximum(),stack.GetMaximum()))
+    stack.SetMaximum(maximum*1.5)
+    #data
+    data.SetMarkerSize(1.0)
+    data.Draw("AP same")
+    s.FixupHisto(data)
+    if leg:
+      leg.Draw("same")
+    # ratio
+    s.cd_ratioPad();
+    s.hratio,s.href = data.Clone("hratio"),data.Clone("href")
+    s.hratio.Divide(stack.GetStack().Last())
+    s.drawRefLine(s.href)
+    s.drawRatio(s.hratio)
+    s.hratio.Draw("AP same");
+    s.update()
+
+  def WAsymmetry(s,hplus,hminus,title='asymmetry'):
+    """ (W+ - W-)/(W+ + W-) workhorse function """
+    hsum = hplus.Clone(hplus.GetName()+" asymmetry")
+    hsum.Add(hminus,-1.0)
+    hdiff = hplus.Clone(hplus.GetName()+" asymmetry_diff")
+    hdiff.Add(hminus)
+    hsum.Divide(hdiff)
+    hsum.SetTitle(title)
+    return hsum
+
+  def plotAsymmetry(s,hdPOS,hdNEG,hmcPOS,hmcNEG,leg=None):
+    """ Wrapper to make a complete plot of charge asymmetry for data vs mc """
+    s.buildRatio();
+    s.cd_plotPad();
+    # mc
+    s.hmcasym = s.WAsymmetry(hmcPOS,hmcNEG)
+    s.hmcasym.SetLineColor(ROOT.kBlue)
+    s.hmcasym.Draw()
+    s.FixupHisto(s.hmcasym)
+    s.hmcasym.GetYaxis().SetRangeUser(0.0,0.5);
+    s.hmcasym.GetYaxis().SetTitle('Asymmetry');
+    # data
+    s.hdasym = s.WAsymmetry(hdPOS,hdNEG)
+    s.hdasym.SetMarkerSize(1.0)
+    s.hdasym.Draw("AP same")
+    s.FixupHisto(s.hdasym)
+    if leg:
+      leg.Draw("same")
+    # ratio
+    s.cd_ratioPad();
+    s.hratio,s.href = s.hdasym.Clone("hratio"),s.hdasym.Clone("href")
+    s.hratio.Divide(s.hmcasym)
+    s.drawRefLine(s.href)
+    s.drawRatio(s.hratio)
+    s.hratio.Draw("AP same");
+    s.update()
+
+  def __init__(s):
+    s._ratioDrawn = False
+
+  def buildDefault(s):
+    s._ratioDrawn = False;
+    s._canvas = ROOT.TCanvas( "canvas" , "default" , 0 , 0 , 800 , 600 );
+    s._canvas.SetFillColor( ROOT.kWhite );
+    s._canvas.GetFrame().SetBorderMode( 0 );
+    s._canvas.GetFrame().SetBorderSize( 0 );
+    s._canvas.SetTopMargin( 0.05 );
+    s._canvas.SetBottomMargin( 0.1 );
+    s._canvas.SetRightMargin( 0.05 );
+    s._canvas.SetLeftMargin( 0.1 );
+    s._canvas.cd();
+    s._plotPad = ROOT.TPad()
+    s._ratioPad = ROOT.TPad()
+    s._coverPad = ROOT.TPad()
+    s._labelSize = 0.025;
+    s._axisTitleSize = 0.025;
+    s._legendTextSize = 0.025;
+    s._legendVertSpacing = 0.03;
+    s._paveTextSize = 0.025;
+    s._xTitleOffset = 1.25;
+    s._yTitleOffset = 2.;
+
+  def update(s):
+    for c in (s._plotPad,s._coverPad,s._ratioPad,s._canvas):
+      c.Modified()
+      c.Update()
+
+  def buildRatio(s):
+    s._canvas = ROOT.TCanvas( "canvas" , "ratio" , 200 , 10 , 800 , 800 );
+    s._canvas.SetFillColor( ROOT.kWhite );
+    s._canvas.GetFrame().SetBorderMode( 0 );
+    s._canvas.GetFrame().SetBorderSize( 0 );
+    s._canvas.SetTopMargin( 0.05 ); # set this so getY2 works
+    s._ratioDrawn = True;
+    # pad containing the main plot + legend
+    s._plotPad = ROOT.TPad( "plotPad" , "plotPad" , 0. , 0. , 1. , 1. );
+    s._plotPad.SetBottomMargin( 0.25 );
+    s._plotPad.SetTopMargin( 0.05 );
+    s._plotPad.SetRightMargin( 0.05 );
+    s._plotPad.SetLeftMargin( 0.1 );
+    s._plotPad.SetFillColor( ROOT.kWhite );
+    s._plotPad.Draw();
+    # pad covering the bottom number
+    s._coverPad = ROOT.TPad( "coverPad" , "coverPad" , 0. , 0.24 , 0.0975 , 0.28 );
+    s._coverPad.SetBottomMargin( 0. );
+    s._coverPad.SetTopMargin( 0. );
+    s._coverPad.SetLeftMargin( 0. );
+    s._coverPad.SetRightMargin( 0. );
+    s._coverPad.SetFillColor( ROOT.kWhite );
+    s._coverPad.Draw();
+    # pad containing ratio histogram
+    s._ratioPad = ROOT.TPad( "ratioPad" , "ratioPad" , 0. , 0. , 1. , 0.25 );
+    s._ratioPad.SetBottomMargin( 0.275 );
+    s._ratioPad.SetTopMargin( 0. );
+    s._ratioPad.SetLeftMargin( 0.1 );
+    s._ratioPad.SetRightMargin( 0.05 );
+    s._ratioPad.SetFillColor( ROOT.kWhite );
+    s._ratioPad.Draw();
+    s._plotPad.cd();
+    
+    s._labelSize = 0.025;
+    s._axisTitleSize = 0.025;
+    s._ratioLabelSize = 0.1;
+    s._ratioAxisTitleSize = 0.1;
+    s._legendTextSize = 0.025;
+    s._legendVertSpacing = 0.03;
+    s._paveTextSize = 0.025;
+    s._xTitleOffset = 1.25;
+    s._yTitleOffset = 2.;
+    s._ratioXtitleOffset = 1.;
+    s._ratioYtitleOffset = 0.5;
+
+  def SaveAs(s,name2,ext):
+    name = re.sub(r'[^\w]', '_', name2)
+    c = s._canvas
+    c.SaveAs('%s.%s'%(name,ext))
+    if ext=='eps':
+        c.SaveAs('%s.%s'%(name,'png'))
+  def saveAs( s, fullname ):
+    """ save the canvas """
+    s._canvas.SaveAs( fullname );
+  def saveAsEps( s, filename ):
+    filename +=  ".eps" 
+    s._canvas.SaveAs( filename );
+  def saveAsPng( s, filename ):
+    filename += ".png"
+    s._canvas.SaveAs( filename.c_str() );
+    
+  def clear(s):
+    """ clear the canvas """
+    s._canvas.Clear()
+
+  # cd into different pads
+  def cd_canvas(s):
+    s._canvas.cd()
+  def cd_plotPad(s):
+    assert s._plotPad
+    s._plotPad.cd()
+  def cd_ratioPad(s):
+    assert s._ratioPad
+    s._ratioPad.cd()
+
+  # get vats
+  def getCanvas(s):
+    return s._canvas
+  def getPlotPad(s):
+    return s._plotPad
+  def getRatioPad():
+    return s._ratioPad
+  def getLabelSize(s):
+    return s._labelSize
+  def getAxisTitleSize(s):
+    return s._axisTitleSize
+  def getLegendTextSize(s):
+    return s._legendTextSize
+  def getLegendVertSpacing(s):
+    return s._legendVertSpacing
+  def getPaveTextSize(s):
+    return s._paveTextSize
+  def getRatioLabelSize(s):
+    return s._ratioLabelSize
+  def getRatioAxisTitleSize(s):
+    return s._ratioAxisTitleSize
+  def getXtitleOffset(s):
+    return s._xTitleOffset
+  def getYtitleOffset(s):
+    return s._yTitleOffset
+  def getRatioXtitleOffset(s):
+    return s._ratioXtitleOffset;
+  def getRatioYtitleOffset(s):
+    return s._ratioYtitleOffset
+  def getY2(s):
+    return 1. - float( s._canvas.GetTopMargin() ) - 0.01;
+  def getY1(s):
+    return float( s._canvas.GetBottomMargin() + 0.01 );
+  def getX1(s):
+    return float( s._canvas.GetLeftMargin() + 0.01 );
+  def getX2(s):
+    return 1. - float( s._canvas.GetRightMargin() ) - 0.01;
