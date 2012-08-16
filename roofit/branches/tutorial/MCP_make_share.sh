6cbff400f1012f0f6eabc60533a95aaab3a6b004
antonk <antonk@639f0546-53f7-3a65-3376-bd91f4a7b4f4>
2012-08-16 19:01:04 +0000

    Updates to MCP scripts: i have a draft code that makes final mc11c corrections

--- roofit/branches/tutorial/MCP_make_share.sh
+++ roofit/branches/tutorial/MCP_make_share.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./db_dumpscales.py -m15 -a0 > ~/mcp/MuonMomentumCorrections/share/Data11_Scale_kpkm_values_staco_Rel17.dat
-./db_dumpscales.py -m15 -a1 > ~/mcp/MuonMomentumCorrections/share/Data11_Scale_kpkm_values_muid_Rel17.dat
+#./db_dumpscales.py -m15 -a0 > ~/mcp/MuonMomentumCorrections/share/Data11_Scale_kpkm_values_staco_Rel17.dat
+#./db_dumpscales.py -m15 -a1 > ~/mcp/MuonMomentumCorrections/share/Data11_Scale_kpkm_values_muid_Rel17.dat
 
 ./db_dumpscales.py -m15 -a0 -s > ~/mcp/MuonMomentumCorrections/share/Data11_Scale_KC_values_staco_Rel17.dat
-./db_dumpscales.py -m15 -a1 -s > ~/mcp/MuonMomentumCorrections/share/Data11_Scale_KC_values_muid_Rel17.dat
+#./db_dumpscales.py -m15 -a1 -s > ~/mcp/MuonMomentumCorrections/share/Data11_Scale_KC_values_muid_Rel17.dat
