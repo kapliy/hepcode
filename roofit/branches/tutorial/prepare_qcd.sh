c87519fa764c83355694dedd156ca1d9e2a6e935
antonk <antonk@639f0546-53f7-3a65-3376-bd91f4a7b4f4>
2012-10-17 22:36:13 +0000

    committing missing scripts

---
+++ roofit/branches/tutorial/prepare_qcd.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Saved qcd histograms for Max
+
+#input=/share/t3data3/antonk/ana/ana_v29G_08182012_max_wpt_wpol_stacoCB_all  # max cutflow + new boson pt & polarization reweighting
+#input=/share/t3data3/antonk/ana/ana_v29G_08182012_max_wpt_wpol_newMCP_stacoCB_all   # same, plus my new MCP corrections
+
+#input=/share/t3data3/antonk/ana/ana_v29G_08242012_allwts_V29G_stacoCB_all  # ALL weights but vxz+ v29g ntuple + jerup/down
+#input=/share/t3data3/antonk/ana/ana_v29G_08242012_nowts_V29I_stacoCB_all  # NO weights
+#input=/share/t3data3/antonk/ana/ana_v29G_08242012_allwts_V29I_stacoCB_all  # ALL weights + v29i ntuple + jerup/down
+#input=/share/t3data3/antonk/ana/ana_v29I_08242012_allwts_wptMC10_stacoCB_all   # ALL weights but using wptMC10 reweighting
+#input=/share/t3data3/antonk/ana/ana_v29I_09082012_allwts_wptPow8_eta_stacoCB_all #changed to powheg8 wpt reweighting. Fixed alpgen. Added eta (vs |eta|) histograms.
+#input=/share/t3data3/antonk/ana/ana_v29I_09082012_allwts_wptPow8_eta_newSF_stacoCB_all # added new reco-level scale factors in measurement bins
+#input=/share/t3data3/antonk/ana/ana_v29I_09272012_allwts_wptPow8_eta_newSFTF_stacoCB_all # added trigger SF, too (from Max)
+input=/share/t3data3/antonk/ana/ana_v29I_09282012_allwts_wptPow8_eta_newSFTFQ_stacoCB_all  # added charge-dependent trigger SF from Max
+input=/share/t3data3/antonk/ana/ana_v29I_1002012_allwts_wptPow8_eta_newSFTFQ_stacoCB_all #fixed
+
+
+bgqcd=3
+name="IN_09282012.newSFTFQ.v1"
+
+bgqcd=4
+name="IN_09282012.newSFTFQ_QCD4.v1"
+
+./stack2.py -m prepare_qcd_2d --input ${input} --qcd 1.0 -b --var "fabs(l_eta)" --bin 10,0.0,2.5 --hsource "lepton_absetav" -o TEST -t TEST --pre "ptiso40/l_pt<0.1 && met>25.0 && l_pt>20.0 && fabs(l_eta)<2.4 && w_mt>40.0 && idhits==1 && fabs(z0)<10.0 && nmuons==1" --cut "mcw*puw*wptw*effw*isow*trigw" --bgsig 5 --bgewk 5 --bgqcd ${bgqcd} --extra "${name}.abseta.2D.root"
+./stack2.py -m prepare_qcd_1d --input ${input} --qcd 1.0 -b --var "fabs(l_eta)" --bin 10,0.0,2.5 --hsource "lepton_absetav" -o TEST -t TEST --pre "ptiso40/l_pt<0.1 && met>25.0 && l_pt>20.0 && fabs(l_eta)<2.4 && w_mt>40.0 && idhits==1 && fabs(z0)<10.0 && nmuons==1" --cut "mcw*puw*wptw*effw*isow*trigw" --bgsig 5 --bgewk 5 --bgqcd ${bgqcd} --extra "${name}.abseta.1D.root"
+
+./stack2.py -m prepare_qcd_2d --input ${input} --qcd 1.0 -b --var "fabs(l_eta)" --bin 10,0.0,2.5 --hsource "lepton_absetav" -o TEST -t TEST --pre "ptiso40/l_pt<0.1 && met>25.0 && l_pt>20.0 && fabs(l_eta)<2.4 && w_mt>40.0 && idhits==1 && fabs(z0)<10.0 && nmuons==1" --cut "mcw*puw*wptw*effw*isow*trigw" --bgsig 5 --bgewk 5 --bgqcd ${bgqcd} --extra "${name}.eta.2D.root" --etamode 1
+./stack2.py -m prepare_qcd_1d --input ${input} --qcd 1.0 -b --var "fabs(l_eta)" --bin 10,0.0,2.5 --hsource "lepton_absetav" -o TEST -t TEST --pre "ptiso40/l_pt<0.1 && met>25.0 && l_pt>20.0 && fabs(l_eta)<2.4 && w_mt>40.0 && idhits==1 && fabs(z0)<10.0 && nmuons==1" --cut "mcw*puw*wptw*effw*isow*trigw" --bgsig 5 --bgewk 5 --bgqcd ${bgqcd}  --extra "${name}.eta.1D.root" --etamode 1
