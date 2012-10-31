712917d819c80e77a1f6cde368cea1e362ae6a40
antonk <antonk@639f0546-53f7-3a65-3376-bd91f4a7b4f4>
2012-10-31 16:54:51 +0000

    new max scripts

---
+++ roofit/branches/tutorial/max_unfold.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Unfolded distributions with systematics.
+# For now, this is using MC-based QCD only
+# Currently, studying specifically MCP scale systematics
+
+input=/share/t3data3/antonk/ana/ana_v29I_10212012_unfold_stacoCB_all  # renamed lepton_pt for consistency
+
+# MAIN PLOTTER
+for q in 0 1; do
+    for ii in ALL {1..7}; do
+	./stack2.py --input ${input} -q $q -b --hsource 'd2_eta_lpt:y:%d:%d' -o TEST -t UNF_Q${q}_${ii} -m unfold2d --bgsig 5 --bgewk 5 --bgqcd 0 --preNN ${ii} &> LOG.unf.Q${q}_${ii} &
+    done
+done
+
+echo "Waiting..."
+wait
+
