945ba4b42bfe7d21ebedf9c7ffcffa1404ac5755
antonk <antonk@639f0546-53f7-3a65-3376-bd91f4a7b4f4>
2012-11-05 04:22:12 +0000

    QCD fits: * Add rebin 4 * Add (untested) histograms in pt slices only * New pbs script that checks slot availability Also, bugfix to legend vs stack size check: an element of the stack may be missing sometimes - e.g., ztautau

--- roofit/branches/tutorial/QCD_2D_submit.sh
+++ roofit/branches/tutorial/QCD_2D_submit.sh
@@ -1,16 +1,41 @@
 #!/bin/bash
+
+# smart script to keep submitting jobs as long as there are queue slots available
+
 ntot=`./QCD_2D.sh | grep Max | awk '{print $3}'`
-echo $ntot
-for i in `seq 0 ${ntot}`; do
-    rem=`expr $i % 400`
-    if [ "$i" -ne "0" -a "$rem" -eq "0" ]; then
-	echo "Current job: ${i}. Sleeping for 1 minute ..."
-	sleep 100
+nperblk=600 # njobs per block
+nsubmin=220 # resubmit when jobs fall below this
+nblk=`expr ${ntot} / ${nperblk}` # total number of blocks
+nperlast=`expr ${ntot} % ${nperblk}` # njobs in last block
+
+echo "Total: ${ntot} Blocks: ${nblk}"
+
+for iblk in `seq 0 ${nblk}`; do
+    ijmin=`expr ${iblk} \* ${nperblk}`
+    ijmax=`expr ${iblk} \* ${nperblk} + ${nperblk} - 1`
+    if [ "${iblk}" == "${nblk}" ]; then
+	ijmax=`expr ${iblk} \* ${nperblk} + ${nperlast} - 1`
     fi
-    jid=`qsub -v id=$i -N QCD2D_${i} -o /home/antonk/roofit/logs/log.QCD2D.${i}.stdout -e /home/antonk/roofit/logs/log.QCD2D.${i}.stderr QCD_2D.sh`
-    if [ "$jid" == "" ]; then
-	echo "Retrying submission: $i"
-	sleep 5
+    # keep waiting until there are enough job slots
+    islp=0
+    while [ "1" -eq "1" ]; do
+	nactive=`qstat -u $USER 2>/dev/null | grep -c $USER`
+	if [ "${nactive}" -lt "${nsubmin}" ]; then
+	    break
+	fi
+	echo "Sleeping sequence: $islp because ${nactive} active jobs exceed ${nsubmin} job limit"
+	((islp++))
+	sleep 20
+    done
+    echo "BLOCK ${iblk} / ${nblk} : `expr ${ijmax} - ${ijmin} + 1` jobs: ${ijmin} to ${ijmax}"
+    # submit jobs in this group
+    for i in `seq ${ijmin} ${ijmax}`; do
 	jid=`qsub -v id=$i -N QCD2D_${i} -o /home/antonk/roofit/logs/log.QCD2D.${i}.stdout -e /home/antonk/roofit/logs/log.QCD2D.${i}.stderr QCD_2D.sh`
-    fi
+	echo "EYING: $i"
+	if [ "$jid" == "" ]; then
+	    echo "Retrying submission: $i"
+	    sleep 10
+	    jid=`qsub -v id=$i -N QCD2D_${i} -o /home/antonk/roofit/logs/log.QCD2D.${i}.stdout -e /home/antonk/roofit/logs/log.QCD2D.${i}.stderr QCD_2D.sh`
+	fi
+    done
 done
