61b7aa47365a9f4901a6dd823810b89e52dc01fd
antonk <kapliy@gmail.com>
2012-11-12 23:51:26 +0000

    Final

--- slim/trunk/run.sh
+++ slim/trunk/run.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 function run_sig() {
-    ./filter-and-merge-d3pd.py -i LIST -o out.root --tree tree --var vars.txt -m 2000
+    ./filter-and-merge-d3pd.py -i LIST -o out_v1_29l.root --tree tree --var vars.txt -m 2000
 }
 
 function run_mc() {
-    ./filter-and-merge-d3pd.py -i LIST -o out.root --tree tree --var vars.txt -m 2000 -s file:sel_mc.py 
+    ./filter-and-merge-d3pd.py -i LIST -o out_v1_29l.root --tree tree --var vars.txt -m 2000 -s file:sel_mc.py 
 }
 
 function run_data() {
-    ./filter-and-merge-d3pd.py -i LIST -o out.root --tree tree --var vars.txt -m 2000 -s file:sel_data.py --grl "/home/antonk/TrigFTKAna/good_run_lists/asym_data11_7TeV.pro10.DtoM.xml"
+    ./filter-and-merge-d3pd.py -i LIST -o out_v1_29l.root --tree tree --var vars.txt -m 2000 -s file:sel_data.py --grl "/home/antonk/TrigFTKAna/good_run_lists/asym_data11_7TeV.pro10.DtoM.xml"
 }
 
-
-run_mc
+run_data
