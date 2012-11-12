bf283c31da5442690fe5fc969b7d2d15a5cf0842
antonk <kapliy@gmail.com>
2012-11-12 21:37:41 +0000

    up

--- slim/trunk/sel.py
+++ slim/trunk/sel.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
-_PTMIN = 13
+_PTMIN = 15
 
 def filter_fct(t):
     trig = ('EF_mu18_MG_medium' in t.trig_ef) or ('EF_mu18_MG' in t.trig_ef)
     if not trig: return False
+    nmu = [i for i in xrange(t.nmu) if t.mu_primauthor[i]==6 and t.mu_pt[i]>_PTMIN]
+    if len(nmu)==0: return False
     return True
