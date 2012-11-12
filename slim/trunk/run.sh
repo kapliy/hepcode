711e7883e9456e9ba0520bc4f45e94ca3ebe3cc8
antonk <antonk@639f0546-53f7-3a65-3376-bd91f4a7b4f4>
2012-11-12 21:41:29 +0000

    ch

--- slim/trunk/run.sh
+++ slim/trunk/run.sh
@@ -5,11 +5,11 @@ function run_sig() {
 }
 
 function run_mc() {
-    ./filter-and-merge-d3pd.py -i LIST -o out.root --tree tree --var vars.txt -m 2000 -s file:sel.py 
+    ./filter-and-merge-d3pd.py -i LIST -o out.root --tree tree --var vars.txt -m 2000 -s file:sel_mc.py 
 }
 
 function run_data() {
-    ./filter-and-merge-d3pd.py -i LIST -o out.root --tree tree --var vars.txt -m 2000 -s file:sel.py --grl "/home/antonk/TrigFTKAna/good_run_lists/asym_data11_7TeV.pro10.DtoM.xml"
+    ./filter-and-merge-d3pd.py -i LIST -o out.root --tree tree --var vars.txt -m 2000 -s file:sel_data.py --grl "/home/antonk/TrigFTKAna/good_run_lists/asym_data11_7TeV.pro10.DtoM.xml"
 }
 
 
--- slim/trunk/sel_data.py
+++ slim/trunk/sel_data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-_PTMIN = 15
+_PTMIN = 19.98
 
 def filter_fct(t):
     trig = ('EF_mu18_MG_medium' in t.trig_ef) or ('EF_mu18_MG' in t.trig_ef)
