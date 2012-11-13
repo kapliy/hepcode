0d5f9d7bb4b6c89c478bc376e7db1a4fb42f7c3b
antonk <kapliy@gmail.com>
2012-11-13 20:10:16 +0000

    added list of paths

--- slim/trunk/run.sh
+++ slim/trunk/run.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-function run_sig() {
-    ./filter-and-merge-d3pd.py -i LIST -o out_v1_29l.root --tree tree --var vars.txt -m 2000
+function run_old() {
+    ./filter-and-merge-d3pd.py -i LIST -o out_v1_29l_old.root --tree tree --var vars.txt -m 2000 --grl "/home/antonk/TrigFTKAna/good_run_lists/asym_data11_7TeV.pro10.DtoM.xml" -s file:sel_data.py
 }
 
-function run_mc() {
-    ./filter-and-merge-d3pd.py -i LIST -o out_v1_29l.root --tree tree --var vars.txt -m 2000 -s file:sel_mc.py 
+function run_new() {
+    ./slim.py -i LIST -o out_v1_29l.root --tree tree --var vars.txt --grl /home/antonk/TrigFTKAna/good_run_lists/asym_data11_7TeV.pro10.DtoM.xml --pt 19.99 --trigger
 }
-
-function run_data() {
-    ./filter-and-merge-d3pd.py -i LIST -o out_v1_29l.root --tree tree --var vars.txt -m 2000 -s file:sel_data.py --grl "/home/antonk/TrigFTKAna/good_run_lists/asym_data11_7TeV.pro10.DtoM.xml"
-}
-
-run_data
+run_new &> LOG.run_new &
+run_old &> LOG.run_old &
+wait
