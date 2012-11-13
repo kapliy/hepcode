c8cbb369b0ef2e5112e4576a78aa3e7a29d82327
antonk <antonk@639f0546-53f7-3a65-3376-bd91f4a7b4f4>
2012-11-13 20:10:16 +0000

    added list of paths

--- slim/trunk/sel_data.py
+++ slim/trunk/sel_data.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 
-_PTMIN = 19.98
+_PTMIN = 19.99
 
 def filter_fct(t):
-    return True
     trig = ('EF_mu18_MG_medium' in t.trig_ef) or ('EF_mu18_MG' in t.trig_ef)
     if not trig: return False
     nmu = [i for i in xrange(t.nmu) if t.mu_primauthor[i]==6 and t.mu_pt[i]>_PTMIN]
