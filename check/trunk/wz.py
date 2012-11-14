14dd0ce323142eec95de1ecf0c000c55d2e56901
antonk <kapliy@gmail.com>
2012-11-14 16:18:40 +0000

    wz.py update

--- check/trunk/wz.py
+++ check/trunk/wz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os,sys
+import os,sys,re
 if not 'libPyRoot' in sys.modules: #hack to get rid of TenvRec warnings
     from ROOT import gROOT
     sys.modules['libPyROOT'].gROOT.GetListOfGlobals().FindObject('gErrorIgnoreLevel').GetAddress().__setitem__(0,3000)
@@ -19,10 +19,13 @@ fname = sys.argv[1]
 if True:
     flist = open(sys.argv[1])
     fname = flist.readlines()[0].strip()
+    
+if re.match("/pnfs/uchicago.edu/atlaslocalgroupdisk/user/",fname):
+    fname = 'root://xrddc.mwt2.org:1096//atlas/dq2/' + re.sub('/pnfs/uchicago.edu/atlaslocalgroupdisk/','',fname)
 
 print fname
 
 f = ROOT.TFile.Open(fname)
 t = f.tree
 #t.Scan('event:mc_pdgid:mc_status:mc_pt')
-t.Scan('event:mc_m:mc_pdgid:mc_status:mc_pt','fabs(mc_pdgid)==23 || fabs(mc_pdgid)==24')
+t.Scan('event:mc_m:mc_pdgid:mc_status:mc_pt','(fabs(mc_pdgid)==23 || fabs(mc_pdgid)==24 || fabs(mc_pdgid)==13)')
