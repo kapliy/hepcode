cdfedb8519538b54fd43a937b9a82dfd79c8f237
antonk <antonk@639f0546-53f7-3a65-3376-bd91f4a7b4f4>
2012-12-18 23:47:29 +0000

    Updated histogram-based stack plotter

--- roofit/branches/tutorial/max_stacks.sh
+++ roofit/branches/tutorial/max_stacks.sh
@@ -13,16 +13,16 @@ if [ "0" == "1" ]; then
 fi
 
 # Standard stacks
+common="-b --input ${input} --lvar 'd3_abseta_lpt_met:x:0:-1:y:0:-1' --lbin 100,0,40 --lrebin 2 -o CONTROL -t P -m control_stack --charge BOTH --bgqcd ${bgqcd}"
 if [ "0" == "1" ]; then
-    ./stack2.py -b --input ${input} --hsource "lepton_etav" --bin 10,-2.5,2.5 --refline 0.92,1.095 -o CONTROL -t P -m control_stack --charge BOTH --bgqcd ${bgqcd} &
-    ./stack2.py -b --input ${input} --hsource "lepton_phi" --bin 10,-3.14,3.14 --rebin 2 --refline 0.92,1.095 -o CONTROL -t P -m control_stack --charge BOTH --bgqcd ${bgqcd} &
-    ./stack2.py -b --input ${input} --hsource "wmt" --bin 100,40,120 --rebin 2 --refline 0.85,1.175 -o CONTROL -t P -m control_stack --charge BOTH --bgqcd ${bgqcd} &
-    ./stack2.py -b --input ${input} --hsource "wpt" --bin 100,0,120  --rebin 2 --refline 0.85,1.175 -o CONTROL -t P -m control_stack --charge BOTH --bgqcd ${bgqcd} &
-    ./stack2.py -b --input ${input} --hsource "met" --bin 100,25,120  --rebin 2 --refline 0.85,1.175 -o CONTROL -t P -m control_stack --charge BOTH --bgqcd ${bgqcd} &
-    ./stack2.py -b --input ${input} --hsource "lpt" --bin 100,0,120  --rebin 2 --refline 0.85,1.175 -o CONTROL -t P -m control_stack --charge BOTH --bgqcd ${bgqcd} --refline 0.91,1.09 &
+    ./stack2.py --hsource "lepton_etav" --bin 10,-2.5,2.5 --refline 0.92,1.095 ${common} &
+    ./stack2.py --hsource "lepton_phi" --bin 10,-3.14,3.14 --rebin 2 --refline 0.92,1.095 ${common} &
+    ./stack2.py --hsource "wmt" --bin 100,40,120 --rebin 2 --refline 0.85,1.175 ${common} &
+    ./stack2.py --hsource "wpt" --bin 100,0,120  --rebin 2 --refline 0.85,1.175 ${common} &
+    ./stack2.py --hsource "met" --bin 100,25,120 --rebin 2 --refline 0.85,1.175 ${common} &
+    ./stack2.py --hsource "lpt" --bin 100,0,120  --rebin 2 --refline 0.85,1.175 ${common} &
 fi
 
-#./stack2.py -b --input ${input} --hsource "met" --bin 100,25,120  --rebin 2 --refline 0.85,1.175 -o CONTROL -t P -m control_stack --charge 0 --bgqcd ${bgqcd}
 ./stack2.py -b --input ${input} --hsource "d3_abseta_lpt_met:x:0:-1:y:0:-1" --bin 100,25,120  --rebin 2 --refline 0.85,1.175 -o CONTROL -t D -m control_stack --charge 0 --bgqcd ${bgqcd} --lvar "d3_abseta_lpt_met:x:0:-1:y:0:-1" --lbin 100,0,40 --lrebin 2
 
 echo "Please wait..."
