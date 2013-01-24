79dcaae0f9fe2b65e22873d943c5e5b86c31581a
antonk <antonk@639f0546-53f7-3a65-3376-bd91f4a7b4f4>
2013-01-24 05:54:06 +0000

    bugfix to cutflow script: it did not erase previous results

--- roofit/branches/tutorial/max_cutflow.sh
+++ roofit/branches/tutorial/max_cutflow.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
 # RUNNING CUTFLOW
-bdir=/share/t3data3/antonk/ana/ana_v29I_12042012_edboard_stacoCB_all
-bdir=/share/t3data3/antonk/ana/ana_v29I_01072013_edboard_stacoCB_all
-bdir=/share/t3data3/antonk/ana/ana_v29I_01152013_paper_stacoCB_all
-
 source config.sh
 bdir=`dirname ${input}`
 
 echo "Input directory: ${bdir}"
 
 if [ "$#" -ge "1" ]; then
-    # INSTRUCTIONS ON HOW TO PREPARE A COMPLETE DATA CUTFLOW FILE
     echo "Merging files for combined cutflow:"
     cd ${bdir}
     for pt in 20 25; do
@@ -25,6 +20,6 @@ else
     out=/home/antonk/SupportingDocument/Wmunu/figures/cutflow
     for pt in 20 25; do
     	sdir=${bdir}/pt${pt}
-	./max_cutflow.py ${sdir} 1 POS ${pt} > ${out}/data_pt${pt}.tex && ./max_cutflow.py ${sdir} 0 POS ${pt} >> ${out}/mc_POS_pt${pt}.tex && ./max_cutflow.py ${sdir} 0 NEG ${pt} >> ${out}/mc_NEG_pt${pt}.tex && echo OK
+	./max_cutflow.py ${sdir} 1 POS ${pt} > ${out}/data_pt${pt}.tex && ./max_cutflow.py ${sdir} 0 POS ${pt} > ${out}/mc_POS_pt${pt}.tex && ./max_cutflow.py ${sdir} 0 NEG ${pt} > ${out}/mc_NEG_pt${pt}.tex && echo OK
     done
 fi
