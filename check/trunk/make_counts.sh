3fe359b873142894ef14bb0e96a8f5acedd0c82a
kapliy <kapliy@gmail.com>
2013-03-19 15:38:41 +0000

    added mcnlo and powheg_herwig samples to make_counts

--- check/trunk/make_counts.sh
+++ check/trunk/make_counts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for d in {D..M} powheg_pythia_wminmunu powheg_pythia_wplusmunu; do
+for d in {D..M} powheg_pythia_wminmunu powheg_pythia_wplusmunu powheg_herwig_wminmunu powheg_herwig_wplusmunu mcnlo_wminmunu mcnlo_wplusmunu; do
     echo "Working on: $d ..."
     ./counts.py $d > COUNTS.${d} &
 done
