2d678dda36e30bc9d29b967df37abf7227d34017
kapliy <kapliy@639f0546-53f7-3a65-3376-bd91f4a7b4f4>
2013-04-08 19:28:12 +0000

    updated latest folders that were not in trunk yet

--- ana/trunk/asym_run.sh
+++ ana/trunk/asym_run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export MODE=pbs
 source asym_common.sh
 
 i=$1
@@ -22,12 +23,12 @@ echo "Submitting data and MC11C ..."
 #submit_sample wasymmetry29I_data_periodJ.dat
 #submit_sample wasymmetry29I_mc_powheg_pythia_wminmunu.dat
 #submit_zmumu_all
-#submit_wmumu
 #submit_mc
-submit_all
+###submit_all
+submit_wmumu_nlo
 
 PT=25
 out=${coredir}/ana_v${ntuple}_${lbl}_${muname[$i]}_all/pt${PT}
 xtra="${common_opts} --muon-offset 1 --muon-type $i --muon-scale 1000"
 submit_wmumu_nlo
-submit_data # for cutflow
+###submit_data # for cutflow
