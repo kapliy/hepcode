0e70e812bf9008581e8c7a9fc760212d6b325546
kapliy <kapliy@639f0546-53f7-3a65-3376-bd91f4a7b4f4>
2013-06-13 22:48:27 +0000

    extra rootcore files

---
+++ ana/trunk/CommonAnalysis/RootCore/RootCore/Packages.h
@@ -0,0 +1,30 @@
+#ifndef ROOTCORE_PACKAGES_H
+#define ROOTCORE_PACKAGES_H
+
+// This file contains one define statement for each package detected by
+// RootCore.  It is meant to allow to write package A in a form that
+// it uses the services of package B when available, but otherwise
+// skips it without failing.  For this to work properly you need to list
+// package B in the PACKAGE_TRYDEP of package A.
+
+#define ROOTCORE_PACKAGE_RootCore
+#define ROOTCORE_PACKAGE_ApplyJetCalibration
+#define ROOTCORE_PACKAGE_JetResolution
+#define ROOTCORE_PACKAGE_ApplyJetResolutionSmearing
+#define ROOTCORE_PACKAGE_BootstrapGenerator
+#define ROOTCORE_PACKAGE_CalibrationDataInterface
+#define ROOTCORE_PACKAGE_egammaAnalysisUtils
+#define ROOTCORE_PACKAGE_GoodRunsLists
+#define ROOTCORE_PACKAGE_TrigMuonEfficiency
+#define ROOTCORE_PACKAGE_IsoIPSF
+#define ROOTCORE_PACKAGE_JetUncertainties
+#define ROOTCORE_PACKAGE_LineShapeTool
+#define ROOTCORE_PACKAGE_MuonMomentumCorrections
+#define ROOTCORE_PACKAGE_MissingETUtility
+#define ROOTCORE_PACKAGE_MuonEfficiencyCorrections
+#define ROOTCORE_PACKAGE_MuonIsolationCorrection
+#define ROOTCORE_PACKAGE_MV1Tagger
+#define ROOTCORE_PACKAGE_PileupReweighting
+#define ROOTCORE_PACKAGE_ScaleFactorProvider
+
+#endif
